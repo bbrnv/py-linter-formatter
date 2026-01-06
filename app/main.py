a9523119ecc465a47e13ffb1371a0592bf6b9644
bbrnv <borislav.barnev@gmail.com>
2026-01-06 11:58:50 +0000

    Reformatted according to flake8 suggestions

--- app/main.py
+++ app/main.py
@@ -1,19 +1,26 @@
 def format_linter_error(error: dict) -> dict:
     # write your code here
-    return {"line": error["line_number"], 
-            "column": error["column_number"], 
-            "message": error["text"],
-            "name": error["code"],
-            "source": error["source"]}
+    return {
+        "line": error["line_number"],
+        "column": error["column_number"],
+        "message": error["text"],
+        "name": error["code"],
+        "source": error["source"],
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
     # write your code here
-    return {"errors": [format_linter_error(error) for error in errors],
-            "file_path": file_path, 
-            "status": "failed" if errors else "passed"}
+    return {
+        "errors": [format_linter_error(error) for error in errors],
+        "file_path": file_path,
+        "status": "failed" if errors else "passed",
+    }
 
 
 def format_linter_report(linter_report: dict) -> list:
     # write your code here
-    return [format_single_linter_file(key, value) for key, value in linter_report.items()]
+    return [
+        format_single_linter_file(key, value)
+        for key, value in linter_report.items()
+    ]
